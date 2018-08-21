dc38993d5b17a8400f5555f61de7a7b760652f51
Andy Bitz <artzbitz@gmail.com>
2018-08-21 22:00:24 +0200

    Fixed typo in function

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,6 @@
 <?php
 
 // set the title and include subpages
-echo rex_view::title(rex_i18n('blogger_title'));
+echo rex_view::title(rex_i18n::msg('blogger_title'));
 $subpage = rex_be_controller::getCurrentPagePart(1);
 include rex_be_controller::getCurrentPageObject()->getSubPath();
